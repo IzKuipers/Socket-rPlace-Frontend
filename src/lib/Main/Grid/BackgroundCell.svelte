cfb161f5832d3c7ead931a6cbaed783e8c57a31a
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-04-08 09:23:20 +0200

    been a while

--- src/lib/Main/Grid/BackgroundCell.svelte
+++ src/lib/Main/Grid/BackgroundCell.svelte
@@ -15,9 +15,30 @@
   export let y = 0;
 
   let highlighted = false;
+  let price = 0;
 
   function highlight() {
+    price = 0;
+
     const grid = generateStar({ x, y }).grid;
+    const field = $PlayGrid;
+
+    let subtract = 0;
+
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < field.length; j++) {
+        if (grid[i].x == field[j].x && grid[i].y == field[j].y) {
+          if (field[j].c == $UserData.color) subtract += 25;
+          else {
+            subtract += 50;
+          }
+        }
+      }
+    }
+
+    let newPrice = 25 * grid.length - subtract;
+
+    price = newPrice;
 
     bgGrid.set(grid);
   }
@@ -49,6 +70,9 @@
   class="cell bg"
   id="{x}x{y}"
   on:mouseenter={highlight}
+  on:mouseleave={() => (price = 0)}
   on:click={place}
   class:highlighted
-/>
+>
+  {price == 0 ? " " : price < 0 ? "0!" : price}
+</div>
